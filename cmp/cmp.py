7db262700a9ed20a079e8bfa93d137e49c00fdcc
enne2 <me@enne2.net>
2019-12-18 16:57:12 +0100

    5th day

--- cmp/cmp.py
+++ cmp/cmp.py
@@ -5,29 +5,43 @@ from gi.repository import Gst, GLib, GObject, Soup
 import json
 
 class Source:
-    def __init__(self):
+    def __init__(self,parent):
+        self.parent = parent
         self.videotestsrc = Gst.ElementFactory.make("videotestsrc")
         self.videobox = Gst.ElementFactory.make("videobox")
         self.capsfilter = Gst.ElementFactory.make("capsfilter")
         self.capsfilter.set_property("caps", Gst.Caps.from_string("video/x-raw,width=50,height=50,framerate=25/1"))
+        self.videotestsrc.set_state(Gst.State.PLAYING)
+        self.capsfilter.set_state(Gst.State.PLAYING)
+        self.videobox.set_state(Gst.State.PLAYING)
+        self.capsfilter.set_property("caps", Gst.Caps.from_string("video/x-raw,width=50,height=50,framerate=25/1"))
+
+
+    def transform(self, x, y, z, w, h):
+        self.videobox.set_property("top", int(x)*-1)
+        self.videobox.set_property("left", int(y)*-1)
+        self.capsfilter.set_property("caps", Gst.Caps.from_string("video/x-raw,width="+w+",height="+h+",framerate=25/1"))
+        return dict(version='Source transformed')
 
     def playit(self):
         self.videotestsrc.set_state(Gst.State.PLAYING)
         self.capsfilter.set_state(Gst.State.PLAYING)
         self.videobox.set_state(Gst.State.PLAYING)
-        print("Printed immediately.")
-        time.sleep(2.4)
-        print("Printed after 2.4 seconds.")
-        self.videobox.set_property("top", 0)
-        self.capsfilter.set_property("caps", Gst.Caps.from_string("video/x-raw,width=50,height=50,framerate=25/1"))
-        for x in range(500):
-            print(50+x)
-            self.videobox.set_property("top", x*-1)
-            self.videobox.set_property("left", x*-1)
-            self.capsfilter.set_property("caps", Gst.Caps.from_string("video/x-raw,width="+str(50+x)+",height="+str(50+x)+",framerate=25/1"))
-            time.sleep(0.1)
 
-class VideoWall(object):
+    def __del__(self):
+
+        self.videobox.link(self.parent.core_pipeline.get_by_name("vmix"))
+        self.parent.core_pipeline.remove(self.capsfilter)
+        self.parent.core_pipeline.remove(self.videotestsrc)
+        self.parent.core_pipeline.remove(self.videobox)
+        self.videotestsrc.set_state(Gst.State.NULL)
+
+        self.capsfilter.set_state(Gst.State.NULL)
+
+        self.videobox.set_state(Gst.State.NULL)
+
+
+class CmpWall(object):
 
     sources = {}
 
@@ -41,45 +55,52 @@ class VideoWall(object):
         #Link Elements
         self.sources[id].videotestsrc.link(self.sources[id].capsfilter)
         self.sources[id].capsfilter.link(self.sources[id].videobox)
-
         self.sources[id].videobox.link(self.core_pipeline.get_by_name("vmix"))
-        self.sources[id].playit()
 
+        self.sources[id].playit()
+        return dict(version='Source added with id: '+str(id))
+
+    def on_request(self, server, msg, path, query, client, *data):
+        #logger.info('on_request %s msg: %s query: %s', path, msg, query)
+        #
+        ret = {}
+        #
+        if path == '/':
+            ret = dict(version='0.1a')
+        elif path.startswith('/add/'):
+            id = int(path.replace('/add/', ''))
+            ret = self.add_source(id)
+        #
+        elif path.startswith('/transform/'):
+            id = int(path.replace('/transform/', ''))
+            print(query)
+            #
+            ret = self.sources[id].transform(query["x"],query["y"],query["z"],query["w"],query["h"])
+        #
+        elif path.startswith('/remove/'):
+            id = int(path.replace('/remove/', ''))
+            #
+            del self.sources[id]
+            ret = dict(confirm='Source removed')
+        else:
+            ret = dict(error='not found')
+        #
+        if ret.get('error'):
+            msg.set_status(404)
+        else:
+            msg.set_status(200)
+        msg.set_response('application/json', Soup.MemoryUse.COPY, json.dumps(ret).encode())
 
     def __init__(self):
         #
         self.server = Soup.Server()
-        def on_request(server, msg, path, query, client, *data):
-            #logger.info('on_request %s msg: %s query: %s', path, msg, query)
-            #
-            ret = {}
-            #
-            if path == '/':
-                ret = dict(version='0.1a')
-            elif path.startswith('/add/'):
-                id = int(path.replace('/add/', ''))
-                self.add_source(id)
-                ret = dict(version='shit')
-            #
-            elif path.startswith('/remove/'):
-                id = int(path.replace('/remove/', ''))
-                #
-                ret = self.remove_publisher(id)
-            else:
-                ret = dict(error='not found')
-            #
-            if ret.get('error'):
-                msg.set_status(404)
-            else:
-                msg.set_status(200)
-            msg.set_response('application/json', Soup.MemoryUse.COPY, json.dumps(ret).encode())
         #
-        self.server.add_handler('/', on_request)
+        self.server.add_handler('/', self.on_request)
         self.server.listen_all(8080, Soup.ServerListenOptions.IPV4_ONLY)
         self.server.run_async()
         #pipeline generation
         self.core_pipeline = Gst.parse_launch("videotestsrc name=videotestsrc is-live=true pattern=black do-timestamp=true ! \
-        capsfilter name=core_caps caps=video/x-raw,width=1024,height=768,framerate=25/1 ! \
+        capsfilter name=core_caps caps=video/x-raw,width=1024,height=768,framerate=25/1 ! timeoverlay ! \
         queue name=queuev max-size-buffers=1 \
         max-size-time=0 max-size-bytes=0 silent=1 leaky=2 ! videomixer name=vmix ! videoconvert ! autovideosink")
 
@@ -88,6 +109,6 @@ class VideoWall(object):
 
 
 Gst.init(None)
-mainclass = VideoWall()
+mainclass = CmpWall()
 loop = GLib.MainLoop()
 loop.run()
